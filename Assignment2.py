adf378b1ec48d43704e0c2e9e546c3dbd8be4789
Blue-JBWK <39659223+Blue-JBWK@users.noreply.github.com>
2018-06-03 04:19:04 +1200

    Add files via upload
    Adding final code for the project

--- Assignment2.py
+++ Assignment2.py
@@ -1,3 +1,4 @@
+import time
 import subprocess
 import argparse
 import sys
@@ -5,7 +6,7 @@ NK = 3.0
 
 
 def get_nodes(symbol, n):
-    for n in range(n):
+    for n in range(1, n + 1):
         nodes = ["{}{}".format(symbol, n)]
     return nodes
 
@@ -15,52 +16,53 @@ def valid_check(input1, input2, input3):
         sys.exit(1)
 
 def programe_input():
-    x = input("Enter the number of the source node")
-    y = input("Enter the number of transit node")
-    z = input("Enter the number of the dest node")
+    x = input("Enter the number of the source node ")
+    y = input("Enter the number of transit node ")
+    z = input("Enter the number of the dest node ")
     return x, y, z
 
 def main():
     "Run the program"
-    #x, y, z = programe_input()
+    x, y, z = programe_input()
 
-    to_write(3, 2, 4)
+    to_write(int(x), int(y), int(z))
 
 def to_write(x, y, z):
     every_nodes = set()
     u_set = set()
     links = []
 
-    file = "lpFile.txt"
+    file = "lpFile.lp"
     output = open(file, "w")
 
-    output.write("Minimize\nr\nSubject to\ndemandConstrains:\n")
+    output.write("Minimize\nr\nSubject to\n")
+                 #"demandConstraints:\n")
     for i in range(x):
         for k in range(z):
             line = ""
             for n in range(y):
-                line += "x{0}{1}{2}+".format(i + 1, n + 1, k + 1)
+                line += "x{0}{1}{2} +".format(i + 1, n + 1, k + 1)
                 every_nodes.add("x{0}{1}{2}".format(i + 1, n + 1, k + 1))
             line = line[:-1]
-            line += "={}".format(i + k + 2)
+            line += "= {}".format(i + k + 2)
             output.write(line + "\n")
 
-    output.write("Three Flow Constant:\n")
+    #output.write("threeFlowConstraints:\n")
     for i in range(x):
         for k in range(z):
             line = ""
             for n in range(y):
-                line += "u{0}{1}{2}+".format(i + 1, n + 1, k + 1)
+                line += "u{0}{1}{2} +".format(i + 1, n + 1, k + 1)
                 u_set.add("u{0}{1}{2}".format(i + 1, n + 1, k + 1))
             line = line[:-1]
-            line += "={}".format(NK)
+            line += "= {}".format(NK)
             output.write(line + "\n")
 
-    output.write("Equal Flow Constants\n")
+    #output.write("equalFlowConstraints\n")
     for i in range(x):
         for k in range(z):
             for n in range(y):
-                line = "3x{0}{1}{2}-{3}u{4}{5}{6} = 0".format(i + 1,
+                line = "3 x{0}{1}{2} - {3} u{4}{5}{6} = 0".format(i + 1,
                                                               n + 1,
                                                               k + 1,
                                                               i + k + 2,
@@ -69,31 +71,31 @@ def to_write(x, y, z):
                                                               k + 1)
                 output.write(line + "\n")
 
-    output.write("Source To Transit Link Constrain\n")
+    #output.write("Source To Transit Link Constrain\n")
     for i in range(x):
         for n in range(y):
             line = ""
             for k in range (z):
-                line += "x{0}{1}{2}+".format(i + 1,n + 1,k + 1)
+                line += "x{0}{1}{2} +".format(i + 1,n + 1,k + 1)
                 every_nodes.add("x{0}{1}{2}".format(i + 1,n + 1,k + 1))
             line = line[:-1]
             line += "-c{0}{1} <= 0".format(i + 1, n + 1)
             links.append("y{0}{1}".format(i + 1, n +1))
             output.write(line + "\n")
 
-    output.write("Transit to destination link constrain: \n")
+    #output.write("Transit to destination link constrain: \n")
     for n in range(y):
         for k in range(z):
             line = ""
             for i in range(x):
-                line += "x{0}{1}{2}+".format(i + 1, n + 1, k + 1)
+                line += "x{0}{1}{2} +".format(i + 1, n + 1, k + 1)
                 every_nodes.add("x{0}{1}{2}".format(i + 1, n + 1, k + 1))
             line = line[:-1]
             line += "-d{0}{1} <= 0".format(n + 1, k + 1)
             links.append("y{0}{1}".format(n + 1, k + 1))
             output.write(line + "\n")
 
-    output.write("Transit node constrain: \n")
+    #output.write("Transit node constrain: \n")
     for n in range(y):
         line = ""
         for i in range(x):
@@ -103,15 +105,15 @@ def to_write(x, y, z):
         line += "-r <= 0"
         output.write(line +"\n")
 
-    output.write("Negative constrain: \n")
+    output.write("Bounds \n")
     for node in list(every_nodes):
-        output.write("{0} >= 0\n".format(node))
-    output.write("r >= 0 \n")
+        output.write("0 <= {0}\n".format(node))
+    output.write("0 <= r\n")
     output.write("Binary\n")
     for u in list(u_set):
         output.write(u + "\n")
 
-    output.write("End")
+    output.write("END")
     output.close()
 
     average, maximum, no_of_links, highest_link, highest_cap = cplex_run(file)
@@ -127,45 +129,66 @@ def to_write(x, y, z):
 
 
 def cplex_run(filename):
+    path = "C:\\Users\\Blue - Meraki\\Desktop\\364\\lpFile.lp"
     num_runs = 10
+    cplex_command = ["cplex", "-c", "read", filename,  "optimize", "display", "solution", "variables", "-"]
+    time_command = ["time", "-p", "cplex", "-c", "read", filename, "optimize"]
+    mark = time.clock
 
-    cplex_command = ["cplex -c read " + filename + " optimize display solution variable-"]
-    time_command = ["time -p cplex -c read " + filename + " optimize"]
-    output = subprocess.run(cplex_command, shell= True, stdout = subprocess.PIPE)
+    total = 0
+    for i in range(num_runs):
+        start = mark()
+        output = subprocess.run(cplex_command, shell=True, stdout=subprocess.PIPE)
+        end = mark()
+        total = (end - start)
+    time_avg = total / 10
     cplex_out = output.stdout.decode("utf-8")
+    #print(cplex_out)
 
-    output = cplex_out.split("Variable Name     Solution Calue\n")[-1]
+
+    output2 = cplex_out.split("\n")
+    #print(output2)
+    start_index = output2.index("Variable Name           Solution Value\r")
+    #length = len(output2[start_index:])
+    #print(output2[start_index:])
     max = 0
     no_of_links = 0
     highest_link = ""
     highest_cap = 0
-    for line in output.split("\n"):
+    for line in output2[start_index + 1:]:
+        line.strip("\r")
+        #print(line)
         line = line.split(" ")
         id = line[0]
         try:
             value = float(line[-1])
         except ValueError:
+
             break
         if id == "r":
             max = value
-        if id.startwith("c") or id.startwith("d"):
+        if id.startswith("c") or id.startswith("d"):
             no_of_links += 1
             if highest_cap < value:
                 highest_cap = value
                 highest_link = id
-
     total = 0
-    for run in range(num_runs):
-        output = subprocess.run(time_command,shell=True,stderr=subprocess.PIPE,stdout=subprocess.PIPE)
-        time_list = output.stderr.decode("utf-8").split("\n")
+    #print(no_of_links)
+    #print(max)
+    #print(highest_cap)
+    #print(highest_link)
+    #for run in range(num_runs):
 
-        user_time = get_time(time_list[1])
-        sys_time = get_time(time_list[2])
-        total = total + user_time + sys_time
+        #output = subprocess.run(time_command, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        #print(run)
+        #time_list = output.stderr.decode("utf-8").split("\n")
+        #user_time = get_time(time_list[1])
+        #sys_time = get_time(time_list[2])
+        #total = total + user_time + sys_time
 
-    average = total / num_runs
+    #average = total / num_runs
 
-    return average, max, no_of_links, highest_link, highest_cap
+    return time_avg, max, no_of_links, highest_link, highest_cap
 
 def get_time(time_string):
     return float(time_string.split(" ")[1])
